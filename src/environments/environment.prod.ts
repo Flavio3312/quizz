ff0b758e0e9f2cb08b4cff6bbe099c4311be4ad7
Flity <63319670+Flavio3312@users.noreply.github.com>
2021-08-09 20:27:50 -0300

    segundo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDOxDIpdKi4f11EW9Xguwq1BhDN9ogyzs4",
+    authDomain: "quizz-d126b.firebaseapp.com",
+    projectId: "quizz-d126b",
+    storageBucket: "quizz-d126b.appspot.com",
+    messagingSenderId: "686652392031",
+    appId: "1:686652392031:web:fe7e6dda6f553954f791ea"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: "AIzaSyDOxDIpdKi4f11EW9Xguwq1BhDN9ogyzs4",
+  authDomain: "quizz-d126b.firebaseapp.com",
+  projectId: "quizz-d126b",
+  storageBucket: "quizz-d126b.appspot.com",
+  messagingSenderId: "686652392031",
+  appId: "1:686652392031:web:fe7e6dda6f553954f791ea"
+  
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
